c50df47cf7fc7a7f15fc6e7dff232890eb609cf2
Felix <53119101+felix9611@users.noreply.github.com>
2024-10-22 18:00:56 -0400

    UPDATE

--- backend-java/src/main/java/com/fixedasset/mapper/InvoiceMapper.java
+++ backend-java/src/main/java/com/fixedasset/mapper/InvoiceMapper.java
@@ -22,30 +22,30 @@ public interface InvoiceMapper extends BaseMapper<Invoice> {
             "mc.name as mcName , loc.place_code as placeCode, loc.place_name as placeName," +
             "loc.address as address, loc.zip_code as zipCode, loc.country as country" +
             " FROM invoice as inv " +
-            "left join `member` as m on inv.member_id = m.id " +
-            "left join `member_class` as mc on m.class  = mc.id " +
+            "left join member as m on inv.member_id = m.id " +
+            "left join member_class as mc on m.class  = mc.id " +
             "left join location as loc on inv.location_id = loc.id ";
     String wrapperSql = "SELECT * from ( " + listQuery + " ) AS q ${ew.customSqlSegment}";
 
     String selectOne = listQuery + " where inv.id = ${id}";
 
-    String queryTotalYearWeek = "SELECT concat(year(created_at), '-', week(created_at)) as yearWeek, sum(total_amount) as total FROM invoice ${ew.customSqlSegment} group by yearWeek;";
+    String queryTotalYearWeek = "SELECT CONCAT(date_part('YEAR', created_at), '-', date_part('MONTH', created_at)) as yearWeek, sum(total_amount) as total FROM invoice ${ew.customSqlSegment} group by CONCAT(date_part('YEAR', created_at), '-', date_part('MONTH', created_at));";
     // String wrapperSqlTotalYearWeek = "SELECT * from ( " + queryTotalYearWeek + " q ${ew.customSqlSegment}";
     @Select(queryTotalYearWeek)
     List<QueryTotalYearWeek> queryTotalYearWeek(@Param("ew") Wrapper<Invoice> queryWrapper);
 
-    String queryCountYearWeek = "SELECT concat(year(created_at), '-', week(created_at)) as yearWeek, count(*) as count FROM invoice ${ew.customSqlSegment} group by yearWeek;";
+    String queryCountYearWeek = "SELECT CONCAT(date_part('YEAR', created_at), '-', date_part('MONTH', created_at)) as yearWeek, count(*) as count FROM invoice ${ew.customSqlSegment} group by CONCAT(date_part('YEAR', created_at), '-', date_part('MONTH', created_at));";
     @Select(queryCountYearWeek)
     List<QueryCountYearWeek> queryCountYearWeek(@Param("ew") Wrapper<Invoice> queryWrapper);
 
     String queryTotalShop = "SELECT loc.place_name as placeName, sum(total_amount) as total " +
-            "FROM invoice as inv left join location as loc on inv.location_id = loc.id  group by location_id;";
+            "FROM invoice as inv left join location as loc on inv.location_id = loc.id  group by loc.place_name;";
    // @Select(queryTotalShop)
     List<QueryTotalShop> queryTotalShop();
 
     List<QueryCountShop> queryCountShop();
 
-    @Select(wrapperSql)
+  //  @Select(wrapperSql)
     Page<InvoiceListDto> listAll(Page page, @Param("ew") Wrapper<Invoice> queryWrapper);
 
     @Select(selectOne)
